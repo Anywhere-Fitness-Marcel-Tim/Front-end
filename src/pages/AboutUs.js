ebc4ee6d5d68153b58c56d97e56ef9f854f9359e
marcel <marcel_rodrgez@yahoo.com>
2022-03-03 09:50:02 -0500

    Finished most of the about page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -5,28 +5,30 @@ function AboutUs() {
   return (
     <div className='about-container'>
       <div className='about-top-container'>
-        <img src={image1} alt='Yoga girl'/>
         <div className='about-top-right'>
-          <h2>Fitness Marketing</h2>
-          <h2>Message Here</h2>
-          <button>Schedule Now!</button>
+          <div className='about-top-right-content'>
+          <h2>Bring The Fitness To You</h2>
+          <h2>Anywhere...</h2>
+          <button className='button-style'>Schedule Now!</button>
+          </div>
         </div>
       </div>
       <span className='about-middle'>
           <div className='what-we-do'>
             <h2>What We Do</h2>
-            <p>Wiggle room blue sky closer to the metal proceduralize we've bootstrapped the model. Translating our vision of having a market leading platfrom canatics exploratory investigation data masking.</p>
+            <p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.</p>
           </div>
           <div className='what-we-stand'>
           <h2>What We Stand For</h2>
-            <p>Wiggle room blue sky closer to the metal proceduralize we've bootstrapped the model. Translating our vision of having a market leading platfrom canatics exploratory investigation data masking.</p>
+            <p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.</p>
           </div>
       </span>
       <span className='about-bottom'>
         <div className='about-bottom-container'>
           <h2>We Do Private Sessions</h2>
-          <button>Learn More</button>
-          <button>Request A Quote!</button>
+          <p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.</p>
+          <button className='button-style'>Learn More</button>
+          <button className='button-style'>Request A Quote!</button>
         </div>
       </span>
     </div>
